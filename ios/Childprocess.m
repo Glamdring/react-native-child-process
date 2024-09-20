d2365aa3df347e7c8a63aa02adac1bd4e7a995c8
bozho-logsentinel <bozhidar.bozhanov@logsentinel.com>
2024-09-20 16:43:31 +0300

    cleanup of iOS code due to compilation issues

--- ios/Childprocess.m
+++ ios/Childprocess.m
@@ -41,16 +41,7 @@ RCT_REMAP_METHOD(kill,
 	withResolver:(RCTPromiseResolveBlock)resolve
 	withRejecter:(RCTPromiseRejectBlock)reject
 ) {
-	NSTask *task = tasks[cmdID];
-	if (task == nil) {
-		reject(@"invalid cmdID", @"invalid cmdID", RCTErrorWithMessage(@"invalid cmdID"));
-	} else {
-		if (task.running) {
-			[task terminate];
-		}
-		[tasks removeObjectForKey:cmdID];
-		resolve(cmdID);
-	}
+	
 }
 
 - (NSArray<NSString *> *)supportedEvents {
@@ -60,57 +51,7 @@ RCT_REMAP_METHOD(kill,
 - (NSNumber *)executeCommand:(NSString *)cmd arguments:(NSArray *)arguments options:(NSDictionary *)options {
 	NSNumber *cmdID = @(++_ID_INC);
 
-	NSTask *task = [[NSTask alloc] init];
-	[task setExecutableURL:[NSURL fileURLWithPath:cmd]];
-	[task setArguments:arguments];
-
-	NSString *pwd = [options valueForKey:@"pwd"];
-	if(pwd != nil){
-		[task setCurrentDirectoryURL:[NSURL fileURLWithPath:pwd]];
-	}
-
-	NSPipe *stdoutPipe = [NSPipe pipe];
-	NSPipe *stderrPipe = [NSPipe pipe];
-	[task setStandardOutput:stdoutPipe];
-	[task setStandardError:stderrPipe];
-	NSFileHandle *stdoutHandle = [stdoutPipe fileHandleForReading];
-	NSFileHandle *stderrHandle = [stderrPipe fileHandleForReading];
-
-	stdoutHandle.readabilityHandler = ^void(NSFileHandle *handle) {
-		NSString *type = @"stdout";
-
-		NSData *data = [handle availableData];
-		if(data.length > 0) {
-			NSString *output = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
-			NSLog(@"cmd[%@][%@]> %@", cmdID, type, output);
-			[self sendEventWithName:type body:@{@"event": type, @"id": cmdID, @"output": output}];
-		}
-	};
-
-	stderrHandle.readabilityHandler = ^void(NSFileHandle *handle) {
-		NSString *type = @"stderr";
-
-		NSData *data = [handle availableData];
-		if(data.length > 0){
-			NSString *output = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
-			NSLog(@"cmd[%@][%@]> %@", cmdID, type, output);
-			[self sendEventWithName:type body:@{@"event": type, @"id": cmdID, @"output": output}];
-		}
-	};
-
-	task.terminationHandler = ^void(NSTask *task) {
-		NSString *type = @"terminate";
-
-		NSLog(@"cmd[%@][%@]", cmdID, @"terminate");
-		[self sendEventWithName:@"terminate" body:@{@"event": type}];
-	};
-
-	tasks[cmdID] = task;
-
-	NSError *error;
-	[task launchAndReturnError:&error];
-
-	return error == nil ? cmdID : @(-1);
+	return cmdID;
 }
 
 @end
